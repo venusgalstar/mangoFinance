0d76e2d1e07c0f776168dfe09aa3e69fc015545b
DESKTOP-V2O0D8E\Hari <topdirector2017@gmail.com>
2022-10-18 01:35:10 +0900

    add dotenv add infura id to update web3modal

--- frontend/src/helpers/web3ModalSetup.js
+++ frontend/src/helpers/web3ModalSetup.js
@@ -1,5 +1,8 @@
+import dotenv from "dotenv";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import Web3Modal from "web3modal";
+
+dotenv.config();
 
 /**
   Web3 modal helps us "connect" external wallets:
@@ -11,10 +14,11 @@ const web3ModalSetup = () =>
       walletconnect: {
         package: WalletConnectProvider,
         options: {
+          infuraId: process.env.REACT_APP_INFURA_ID, // required
           rpc: {
             97: "https://data-seed-prebsc-1-s1.binance.org:8545/", // bsctestnet
             //  56: "https://bsc-dataseed.binance.org",
-             //4002: "https://rpc.testnet.fantom.network/",
+            //4002: "https://rpc.testnet.fantom.network/",
           },
         },
       },
